687f1bf1823328c95fa55001a863bb8c25fd28fa
Øyvind Robertsen <oyvind@arcticstyle.net>
2016-04-14 13:24:34 +0200

    Add more plots

--- stats/ghbdc/make_avg_plot.sh
+++ stats/ghbdc/make_avg_plot.sh
@@ -6,20 +6,20 @@ echo -e "#Degree\tAvg" >> avgs.txt;
 for i in {1..7};
 do
   echo -ne "${i}\t" >> avgs.txt;
+  
   cat stats_deg${i}.txt | \
     sed -n 212p | \
-    sed -E 's/^.*([0-9]+\.[0-9]+)/\1/' >> avgs.txt;
+    sed -E 's/^.*([0-9]+\.[0-9]+)/\1/' >> avgs.txt
 done;
-
 
 gnuplot << EOF
 set term epslatex
-set output "seqavg.tex"
+set output "ghbdcavg.tex"
 unset key
-set title "Sequential prefetching"
+set title "Performance of GHB-based DC/RPT/SDP hybrid prefetcher"
 set xlabel "Prefetching degree"
 set ylabel "Average speedup"
 set boxwidth 0.5
 set style fill solid
-plot [][0.95:1.05] "avgs.txt" using 1:2:xtic(1) with boxes
+plot [][0.95:1.1] "avgs.txt" using 1:2:xtic(1) with boxes
 EOF
---
+++ stats/rpt/make_plot.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+rm -f data.txt;
+
+cat stats.txt | \
+  sed -n 186,197p | \
+  sed -E 's/^[[:blank:]]*(([[:alnum:]]|\_)*)[[:blank:]]+[[:digit:]]\.[[:digit:]]+[[:blank:]]+([[:digit:]]\.[[:digit:]]+)[[:blank:]].*/\3 \1/' | \
+  sed -E 's/_/\-/g' | \
+  tr " " "\t" | \
+  awk '{printf "%d\t%s\n", NR, $0}' >> data.txt;
+
+gnuplot <<EOF
+set term epslatex
+set output "rpt.tex"
+unset key
+set title "Reference Prediction Table"
+set ylabel "Speedup"
+set xlabel "Benchmark"
+set boxwidth 0.5
+set style fill solid
+set xtics out nomirror rotate
+plot [][0.5:1.25] "data.txt" using 1:2:xtic(3) with boxes
+EOF
